63c2cc54272c3d5f192d62f870cbf80c65063204
shulme801 <shulme801@gmail.com>
2018-04-07 17:13:11 -0400

    7 April Updated first react app

--- first-react/src/App.js
+++ first-react/src/App.js
@@ -6,9 +6,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hi, I'm a React App</h1>
+        <h1>Hi, I'm a React App!</h1>
       </div>
     );
+    // return React.createElement('div',{className: 'App'},React.createElement('h1',null,'I\'ve been rendered!!'));
   }
 }
 
