c08beccd09b72993ff9175acc772b084180b8551
shulme801 <shulme801@gmail.com>
2018-04-07 15:51:29 -0400

    7 April Next Gen JS code examples

---
+++ src/person-es7.js
@@ -0,0 +1,23 @@
+class Human {
+    
+    gender = 'male';
+    
+    printGender = () => {
+        console.log(this.gender);
+    }
+}
+
+
+class Person extends Human {
+    
+    name = 'Stephen';
+
+
+    printMyName = () => {
+        console.log(this.name);
+    }
+}
+
+const person = new Person();
+person.printMyName();
+person.printGender();
---
+++ src/person.js
@@ -0,0 +1,25 @@
+class Human {
+    constructor() {
+        this.gender = 'male';
+    }
+    
+    printGender() {
+        console.log(this.gender);
+    }
+}
+
+
+class Person extends Human {
+    constructor() {
+        super();
+        this.name = 'Stephen';
+    }
+
+    printMyName() {
+        console.log(this.name);
+    }
+}
+
+const person = new Person();
+person.printMyName();
+person.printGender();
