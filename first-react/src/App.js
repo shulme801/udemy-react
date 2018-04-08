d38cf28f7b599247839f40c67eb9a1ce5533d27a
shulme801 <shulme801@gmail.com>
2018-04-08 09:05:35 -0400

    8 April React Components

--- first-react/src/App.js
+++ first-react/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App!</h1>
+        <p>This is really working!</p>
+        <Person />
       </div>
     );
     // return React.createElement('div',{className: 'App'},React.createElement('h1',null,'I\'ve been rendered!!'));
