6f193839789065eb27d8fbe528f17e683abb5795
shulme801 <shulme801@gmail.com>
2018-05-03 17:56:33 -0400

    3 May Playing with ES8

---
+++ new_es_test.js
@@ -0,0 +1,11 @@
+const cars = { BMW: 3, Tesla: 2, Toyota:1 };
+
+//ES2015
+//Instead of...
+const vals = Object.keys(cars).map(key => cars[key]);
+console.log(vals);
+
+//ES2017 and onwards
+//use ...
+const values = Object.values(cars);
+console.log(values);
