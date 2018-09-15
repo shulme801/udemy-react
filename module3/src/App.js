5c1103fa0ff69e517c7d614a8423ad0490839f27
shulme801 <shulme801@gmail.com>
2018-09-15 13:08:00 -0400

    15 Sept

--- module3/src/App.js
+++ module3/src/App.js
@@ -4,13 +4,13 @@ import Person from './Person/Person';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
+    return <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
         <Person />
-      </div>
-    );
+        <Person /> 
+        <Person />
+      </div>;
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, Does This Work Now?'));
   }
 }
