dc34cc4e02465b7e73a19334375282a7ed15a4b9
shulme801 <shulme801@gmail.com>
2018-09-22 18:03:13 -0400

    22 Sept C

--- module3/src/App.js
+++ module3/src/App.js
@@ -13,14 +13,24 @@ class App extends Component {
     otherState: 'some other value'
   }
   
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     //console.log('Was Clicked');
     // Don't do this: this.state.persons[0].name = 'Maximilian';
     this.setState({
       persons: [
-        { name: "Steve", age: 65 },
-        { name: "Gloria", age: "Forever Young!" },
-        { name: "Manu", age: 30 }
+        { name: newName, age: 65 },
+        { name: "Manu", age: 30 },
+        { name: "Gloria", age: "Forever Young!" }
+      ]
+    });
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Stephen', age: 65 },
+        { name: event.target.value, age: 39},
+        { name: 'Gloria', age: "Forever Young!" }
       ]
     });
   }
@@ -29,23 +39,26 @@ class App extends Component {
     return <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button onClick={() => {
+            this.switchNameHandler("Stephen!");
+          }}>Switch Name
+        </button>
         <Person 
           name={this.state.persons[0].name} 
-          age={this.state.persons[0].age}>
+          age={this.state.persons[0].age} >
         </Person>
         <Person 
           name={this.state.persons[1].name} 
           age={this.state.persons[1].age}
-          click={this.switchNameHandler} >
-          My Hobbies: Growing Many Plants
+          click={this.switchNameHandler.bind(this, 'Stephen!!')}
+          changed={this.nameChangedHandler} >My Hobbies: Racing
         </Person>
         <Person 
           name={this.state.persons[2].name} 
-          age={this.state.persons[2].age}>
-          My Hobbies: Racing 
+          age={this.state.persons[2].age} >
+          My Hobbies: Growing Many Plants
         </Person>
-      </div>
+      </div>;
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, Does This Work Now?'));
   }
 }
