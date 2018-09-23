246646c20d7a660520e67821517b90a9d2b8a4fe
shulme801 <shulme801@gmail.com>
2018-09-23 14:55:18 -0400

    23 Sept Remove old Assignment 1

--- module3/src/App.js
+++ module3/src/App.js
@@ -36,27 +36,31 @@ class App extends Component {
   }
   
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
     return <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={() => {
-            this.switchNameHandler("Stephen!");
-          }}>Switch Name
+        <button 
+            style={style}
+            onClick={() => {
+              this.switchNameHandler("Stephen!");
+            } }
+          >Switch Name
+      
         </button>
-        <Person 
-          name={this.state.persons[0].name} 
-          age={this.state.persons[0].age} >
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler.bind(this, "Stephen!!")} 
+        changed={this.nameChangedHandler}>
+          My Hobbies: Racing
         </Person>
-        <Person 
-          name={this.state.persons[1].name} 
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Stephen!!')}
-          changed={this.nameChangedHandler} >My Hobbies: Racing
-        </Person>
-        <Person 
-          name={this.state.persons[2].name} 
-          age={this.state.persons[2].age} >
-          My Hobbies: Growing Many Plants
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>
+          My Hobbies: Growing Many Many Plants
         </Person>
       </div>;
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, Does This Work Now?'));
